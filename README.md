c3dd39bdbcc490f3b06f9c281f9744f8e2845af1
shardul <shardul.aeer@somaiya.edu>
2021-07-02 11:32:25 +0530

    feat: Find available liquidity in interval
    Implemented code and documented shortcomings

---
+++ README.md
@@ -0,0 +1,7 @@
+# About
+Testing an interval tree crate to find liquidity per tick for cyclos.
+
+# Issues
+1. Can't delete items
+2. Unmaintained
+3. To find total liquidity for a price point, we need to loop through the results. This is inefficient since liquidity for several ticks needs to be found.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use interval_tree::IntervalTree;
+
+#[derive(Debug)]
+struct LiquidityPosition {
+    pub address: u32,
+    pub coin_amount: u32,
+    pub pc_amount: u32
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pos_1 = LiquidityPosition {
+        address: 0,
+        coin_amount: 100,
+        pc_amount: 100
+    };
+
+    let pos_2 = LiquidityPosition {
+        address: 1,
+        coin_amount: 200,
+        pc_amount: 300
+    };
+
+    let mut tree = IntervalTree::new();
+
+    tree.insert(10..=100, pos_1);
+    tree.insert(40..=80, pos_2);
+
+    let mut coin_amount_0 = 0;
+    for entry in tree.find(50..=50) {
+        println!("{:?} => {:?}", entry.key(), entry.get());
+        coin_amount_0 += entry.get().coin_amount;
+    }
+
+    println!("Coin amount at 50: {}", coin_amount_0);
 }
